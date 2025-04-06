dcf096f6bb8c437d1939fd8bfa9e656a7809bd4d
Nicole Loughnane <nicoledawn49@gmail.com>
2025-04-05 20:01:47 -0700

    add more images; edit descriptions

--- src/App.vue
+++ src/App.vue
@@ -1,137 +1,108 @@
 <template>
 
   <!--profile picture and about me-->
-  <div class="intro">
-    <!-- <img class="profile" src="./assets/nicoleprofilecircle.png" /> -->
-    <img class="profile" src="./assets/profile-picture-circle.png" />
-    <div class="flex-col ml-6">
-      <h2 class="text-3xl mb-3">Hello!</h2>
-      <p class="text-base">
-        Hello, I'm Nicole Buckley! I am a UX/UI designer currently working in the defense and intelligence
-        space. I specialize in pioneering new frontend projects to improve user experiences by modernizing legacy UIs,
-        bug squashing, and more! My passion lies in
-        learning user needs and translating them into user-centered experiences and well-rounded documentation.
+  <div class="main">
+    <div class="intro">
+      <!-- <img class="profile" src="./assets/nicoleprofilecircle.png" /> -->
+      <img class="profile" src="./assets/profile-picture-circle.png" />
+      <div class="flex-col ml-6">
+        <h2 class="text-3xl mb-3">Hello I'm Nicole Buckley!</h2>
+        <p class="text-base">
+          I am a UX/UI engineer currently working in the defense and intelligence
+          space. I specialize in pioneering new frontend projects to improve user experiences by modernizing legacy UIs,
+          applying user feedback, and more! My passion lies in
+          learning user needs and translating them into user-centered experiences and well-rounded documentation.
+        </p>
+        <h2 class="text-2xl my-4">About Me</h2>
+        <p class="text-base my-4">
+          My journey in the realm of computer science started in college when I decided I wanted to combine
+          my interests in art and computers into UX design. Currently, I reside in Phoenix, AZ where I spend my days
+          playing video games, drawing, and spending time with
+          my family.
+        </p>
+      </div>
+    </div>
+
+    <!--projects-->
+    <div class="mx-20 mt-8">
+      <h2 class="text-3xl my-4">My Work</h2>
+      <!--dfj-->
+      <!--description-->
+      <h3 class="text-xl my-4 font-bold">Daily Faith Journal</h3>
+      <h3 class="text-base italic text-brand-lightblue">September 2023 - April 2023</h3>
+      <div class="flex justify-center">
+        <img src="./assets/dailyfaithjournal/dfj-logo.png" />
+      </div>
+      <p class="text-base mt-4 mb-8">
+        My CST 452 Senior Capstone Project at Grand Canyon University, called Daily
+        Faith Journal, was built with the MEVN stack (MongoDB, Express, Vue, and Node).
+        In Daily Faith Journal,
+        users have the ability to
+        create journal entries and record sermon notes taken a church through a
+        convenient online platform. The app is designed to be accessible and responsive through
+        the web browser on either a computer or a mobile device.
       </p>
-      <h2 class="text-2xl my-4">About Me</h2>
+      <div class="project-grid">
+        <img class="project-grid-item" src="./assets/dailyfaithjournal/dfj-help-center.png" />
+        <img class="project-grid-item pl-28" src="./assets/dailyfaithjournal/dfj-color-palette.png" />
+        <!-- <img class="project-grid-item" src="./assets/dailyfaithjournal/dfj-login-web.png" /> -->
+        <img class="project-grid-item pl-20" src="./assets/dailyfaithjournal/dfj-update-journal-entry.png" />
+      </div>
+      <div class="pt-10 pb-10">
+        <a class="text-base my-4 text-brand-lightblue" href="https://github.com/nicoleloughnane/CST452Capstone"
+          target="_blank" title="GitHub source code">
+          Check out the project on GitHub
+        </a>
+      </div>
+      <hr />
+      <!--Podcast Prototype project-->
+      <!--description-->
+      <h3 class="text-xl my-4 font-bold">Podcast Prototype Application</h3>
       <p class="text-base my-4">
-        Both within and outside of my software development courses at Grand
-        Canyon University, I enjoy exploring UI/UX development as I am
-        interested in the design of programs and how users interact with them. I
-        am currently building my senior capstone project which is using the MEVN
-        technology stack (MongoDB, Express, Vue, and Node.js).
+        In my User Experience Design (DDN-240) class at Grand Canyon University,
+        our task was to design a mobile application for podcast listening using
+        Adobe XD. We also did user testing in Maze to determine how intuitive our
+        designs were
       </p>
+      <div class="project-grid pb-10">
+        <img class="project-grid-item" src="./assets/uxclass/LoginMain.png" alt="Login wireframe" />
+        <!-- <img class="wireframe" src="./assets/uxclass/recovery.png" alt="Account Recovery wireframe" /> -->
+        <img class="project-grid-item" src="./assets/uxclass/OneGenre.png" alt="one genre wireframe" />
+        <img class="project-grid-item" src="./assets/uxclass/Search.png" alt="Search wireframe" />
+      </div>
+      <hr />
+      <!--WIP: Wilcox VR Project-->
+      <h3 class="text-xl my-4 font-bold">Wilcox VR Project</h3>
+      <h3 class="text-base italic text-brand-lightblue">April 2023</h3>
+
+      <p class="text-base my-4 pb-10">
+        In my Human Computer Interaction and Communication Class (CST-320) my class of 11 students collaborated with
+        a museum in Wilcox, AZ
+        to make a VR game for them. They desire to immerse their visitors in the experience of what Wilcox was like back
+        in the
+        1880s. The initial phase of this project was completed in April 2023. My role was on the a small team where we
+        worked to build a telegraph station.
+        We used Unity and C# to develop the game, and Oculus Quest 2 headsets play it.
+      </p>
+      <hr />
+
+      <!--C# Minesweeper-->
+      <h3 class="text-xl mt-4 font-bold">Minesweeper Game in C#</h3>
+      <h3 class="text-base italic text-brand-lightblue mt-2 mb-4">Grand Canyon University CLC (Group Project)</h3>
+      <div class="minesweeper mb-4 flex justify-center">
+        <img class="minesweeperSnippet" src="./assets/minesweeper/minesweeper.png" alt="Minesweeper Board" />
+      </div>
+
+      <!--description-->
+      <p class="text-base my-4">
+        In my C# III class we learned how to take an implementation of the game Minesweeper from a console with no user
+        interface to a web based game in a group project. This was the first class where I worked a team to build an
+        application.
+        The requirements for the project were a user login/registration system, a minesweeper game, and a way to save
+        game progress for the user to come back later.
+      </p>
+
     </div>
-  </div>
-  <!--technologies and logos-->
-  <!-- <div class="mx-20">
-    <h3 class="text-xl my-4">Technologies I Use</h3>
-    <div class="technologies">
-      <img class="logo" src="./assets/logos/js.png" alt="javascript logo" />
-      <img class="logo" src="./assets/logos/java.png" alt="java logo" />
-      <img class="logo" src="./assets/logos/c-sharp.png" alt="c# logo" />
-      <img class="logo" src="./assets/logos/html.png" alt="html logo" />
-      <img class="logo" src="./assets/logos/css-3.png" alt="css logo" />
-      <img class="logo" src="./assets/logos/xd.png" alt="adobe xd logo" />
-      <img class="logo" src="./assets/logos/mysql.png" alt="mysql logo" />
-      <img class="h-20 w-12 ml-3" src="./assets/logos/mongodb.png" alt="mongodb logo" />
-      <img class="logo" src="./assets/logos/node-js.png" alt="node js logo" />
-      <img class="logo" src="./assets/logos/vuelogo.png" alt="vue logo" />
-      <img class="logo" src="./assets/logos/angular.png" alt="angular logo" />
-      <img class="logo" src="./assets/logos/react.png" alt="react logo" />
-    </div>
-  </div> -->
-
-  <!--projects-->
-  <div class="mx-20 mt-8">
-    <h2 class="text-3xl my-4">Projects</h2>
-    <!--dfj-->
-    <!-- <img class="logical" src="./assets/dailyfaithjournal/logicaldiagram.png" alt="Logical diagram of dfj" /> -->
-    <!--description-->
-    <h3 class="text-xl my-4 font-bold">Daily Faith Journal</h3>
-    <h3 class="text-base italic text-brand-lightblue">September 2023 - April 2023</h3>
-    <img src="./assets/dailyfaithjournal/dfj-logo.png" />
-    <p class="text-base mt-4 mb-8">
-      My CST 452 Senior Capstone Project at Grand Canyon University, called Daily
-      Faith Journal, was built with the MEVN stack (MongoDB, Express, Vue, and Node).
-      In Daily Faith Journal,
-      users have the ability to
-      create journal entries and record sermon notes taken a church through a
-      convenient online platform. The app is designed to be accessible and responsive through
-      the web browser on either a computer or a mobile device.
-    </p>
-    <a class="text-base my-4 text-brand-lightblue" href="https://github.com/nicoleloughnane/CST452Capstone"
-      target="_blank" title="GitHub source code">
-      Check out the project on GitHub
-    </a>
-
-    <!--Podcast Prototype project-->
-    <div class="wireframes">
-      <img class="wireframe" src="./assets/uxclass/LoginMain.png" alt="Login wireframe" />
-      <!-- <img class="wireframe" src="./assets/uxclass/recovery.png" alt="Account Recovery wireframe" /> -->
-      <img class="wireframe" src="./assets/uxclass/OneGenre.png" alt="one genre wireframe" />
-      <img class="wireframe" src="./assets/uxclass/Search.png" alt="Search wireframe" />
-    </div>
-    <!--description-->
-    <h3 class="text-xl my-4 font-bold">Podcast Prototype Application</h3>
-    <a class="text-base my-4 text-brand-lightblue"
-      href="https://xd.adobe.com/view/f22b02c0-dffd-4592-8f1e-891db0753ed1-a778/" target="_blank"
-      title="AdobeXD Prototype">
-      Check out the prototype here
-    </a>
-    <p class="text-base my-4">
-      In my User Experience Design (DDN-240) class at Grand Canyon University,
-      our task was to design a mobile application for podcast listening using
-      Adobe XD. We also did user testing in Maze to determine how intuitive our
-      designs were
-    </p>
-
-    <!--WIP: Wilcox VR Project-->
-    <h3 class="text-xl my-4 font-bold">Wilcox VR Project</h3>
-    <h3 class="text-base italic text-brand-lightblue">April 2023</h3>
-
-    <p class="text-base my-4">
-      In my Human Computer Interaction and Communication Class (CST-320) my class of 11 students collaborated with
-      a museum in Wilcox, AZ
-      to make a VR game for them. They desire to immerse their visitors in the experience of what Wilcox was like back
-      in the
-      1880s. The initial phase of this project was completed in April 2023. My role was on the a small team where we
-      worked to build a telegraph station.
-      We used Unity and C# to develop the game, and Oculus Quest 2 headsets play it.
-    </p>
-
-    <!--C# Minesweeper-->
-    <h3 class="text-xl mt-4 font-bold">Minesweeper Game in C#</h3>
-    <h3 class="text-base italic text-brand-lightblue mt-2 mb-4">Grand Canyon University CLC (Group Project)</h3>
-    <div class="minesweeper mb-4">
-      <img class="minesweeperSnippet" src="./assets/minesweeper/minesweeper.png" alt="Minesweeper Board" />
-      <img class="minesweeperSnippet" src="./assets/minesweeper/codesnippet.png" alt="Minesweeper Board Code Snippet" />
-      <img class="minesweeperSnippet" src="./assets/minesweeper/savegame.png" alt="Save Game" />
-    </div>
-
-    <!--description-->
-    <h3 class="text-base font-bold">Key Technologies and Concepts</h3>
-    <ul class="list-disc ml-9">
-      <li>.NET</li>
-      <li>REST API</li>
-      <li>MVC Architecture</li>
-      <li>Object Oriented Programming</li>
-      <li>Recursion</li>
-      <li>Agile Project Management</li>
-    </ul>
-    <p class="text-base my-4">
-      In my C# III class we learned how to take an implementation of the game Minesweeper from the console to the web in
-      a
-      group project. To do
-      this we went through 2 C# classes prior to this where we learned C# as a language, how to make console apps, and
-      win
-      form apps. The next course taught object oriented concepts along with some more advanced subjects like recursion
-      (also
-      taught in our Java courses). This class was my first in which I worked a team to accomplish the task at hand and
-      since then I have had many group projects which I enjoy doing so that we can efficiently deliver sprints while
-      also
-      working on our strengths that we can bring to the project as a whole.
-    </p>
-
   </div>
 </template>
 
@@ -156,8 +127,6 @@ export default {
 
 .intro {
   display: flex;
-  margin-left: 160px;
-  margin-right: 160px;
   margin-top: 40px;
   margin-bottom: 40px;
 }
@@ -181,18 +150,24 @@ export default {
   height: 60px;
 }
 
-/*the group of wireframes */
-.wireframes {
+/*grid of images */
+.project-grid {
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(450px, 1fr));
   grid-gap: 20px;
 }
 
-/* one wireframe */
-.wireframe {
+/* one image */
+.project-grid-item {
   max-width: 100%;
   height: 350px;
 }
+
+/* color palette */
+/* .project-grid-color-palette {
+  padding-left: 80px;
+} */
+
 
 /*group of snippets from minesweeper project */
 .minesweeper {
