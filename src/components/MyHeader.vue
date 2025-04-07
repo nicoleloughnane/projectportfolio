c9b52a79eee0253595b9661e4acb532d46b9c35c
Nicole Loughnane <nicoledawn49@gmail.com>
2025-04-06 18:00:23 -0700

    use github svg

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="fixed w-full py-4 px-8 flex justify-end items-center">
         <a href="./assets/Nicole_Buckley_Resume.pdf" download
-            class="text-white hover:text-gray-200 transition-colors duration-300 font-medium">
+            class="text-white hover:text-gray-300 transition-colors duration-300 font-medium">
             Resume
         </a>
     </header>
