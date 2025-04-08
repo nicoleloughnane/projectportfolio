b5b3b1076d570ceb18929515936d855235ea4e68
Nicole Loughnane <nicoledawn49@gmail.com>
2025-04-07 19:48:26 -0700

    responsiveness overhaul

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,8 @@
         <h2 class="text-2xl my-4">About Me</h2>
         <p class="text-base my-4">
           My journey in the realm of computer science started in college when I decided I wanted to combine
-          my interests in art and computers into frontend development as well as UI design. Currently, I reside in Phoenix, AZ where I spend my free
+          my interests in art and computers into frontend development as well as UI design. Currently, I reside in
+          Phoenix, AZ where I spend my free
           time
           playing video games, drawing, and spending time with
           my family.
@@ -24,36 +25,40 @@
     </div>
 
     <!--projects-->
-    <div class="mx-20 mt-8">
+    <div class="my-work">
       <h2 class="text-3xl my-4">My Work</h2>
       <!--dfj-->
       <h3 class="text-xl my-4 font-bold">Daily Faith Journal</h3>
       <h3 class="text-base italic text-brand-lightblue">September 2023 - April 2023</h3>
       <div class="intro">
         <img class="dfj-logo" src="./assets/dailyfaithjournal/dfj-logo.png" />
-      <div class="flex-col ml-6">
-      <p class="text-base mt-4 mb-8">
-        My CST 452 Senior Capstone Project at Grand Canyon University, called Daily
-        Faith Journal, was built with the MEVN stack (MongoDB, Express, Vue, and Node).
-        In Daily Faith Journal,
-        users have the ability to
-        create journal entries and record sermon notes taken a church through a
-        convenient online platform. The app is designed to be accessible and responsive through
-        the web browser on either a computer or a mobile device.
-        <br/>
-        <br/>
-        In this class, I singlehandedly saw this project through its initial planning stages, proposal, wireframing,
-        development, and
-        deployment. The first semester of the course was spent planning and providing our professor with a mock proposal
-        for the project. I defined the scope and requirements and how the app would address a user need and help resolve
-        their problem. I planned tickets and sprints using Jira. The second semester of the course was spent completing
-        those tickets by developing the project. I started with low fidelity wireframes in pencil and paper then
-        translated them to high fidelity wireframes in Adobe XD (as displayed below) as well as a
-        color palette to give my app and overall theme. I based the theme off of my university's colors (purple and
-        white). Then, I implemented those wireframes using Vue.js and Tailwind CSS. I used a mock backend to start but
-        then connected the project to a backend I built using Express and MongoDB for my database.
-      </p>
-      </div>
+        <div class="flex-col ml-6">
+          <p class="text-base mt-4 mb-8">
+            My CST 452 Senior Capstone Project at Grand Canyon University, called Daily
+            Faith Journal, was built with the MEVN stack (MongoDB, Express, Vue, and Node).
+            In Daily Faith Journal,
+            users have the ability to
+            create journal entries and record sermon notes taken a church through a
+            convenient online platform. The app is designed to be accessible and responsive through
+            the web browser on either a computer or a mobile device.
+            <br />
+            <br />
+            In this class, I singlehandedly saw this project through its initial planning stages, proposal, wireframing,
+            development, and
+            deployment. The first semester of the course was spent planning and providing our professor with a mock
+            proposal
+            for the project. I defined the scope and requirements and how the app would address a user need and help
+            resolve
+            their problem. I planned tickets and sprints using Jira. The second semester of the course was spent
+            completing
+            those tickets by developing the project. I started with low fidelity wireframes in pencil and paper then
+            translated them to high fidelity wireframes in Adobe XD (as displayed below) as well as a
+            color palette to give my app and overall theme. I based the theme off of my university's colors (purple and
+            white). Then, I implemented those wireframes using Vue.js and Tailwind CSS. I used a mock backend to start
+            but
+            then connected the project to a backend I built using Express and MongoDB for my database.
+          </p>
+        </div>
       </div>
       <h3 class="text-xl my-4">Wireframes</h3>
       <div class="project-flex">
@@ -139,10 +144,14 @@ export default {
 </script>
 
 <style>
-/*profile picture */
+/* profile picture */
 .profile {
   width: 220px;
   height: 220px;
+  aspect-ratio: 1 / 1;
+  max-width: 220px;
+  min-width: 220px;
+  display: block;
 }
 
 .intro {
@@ -151,7 +160,19 @@ export default {
   margin-bottom: 40px;
 }
 
-/* one image */
+.my-work {
+  margin-left: 5rem
+    /* 80px */
+  ;
+  margin-right: 5rem
+    /* 80px */
+  ;
+  margin-top: 2rem
+    /* 32px */
+  ;
+}
+
+/* project image styles */
 .project-flex-item {
   width: 100%;
   height: auto;
@@ -167,7 +188,32 @@ export default {
   gap: 20px;
 }
 
-@media screen and (max-width:1800px) {
+/* minesweeper layout */
+.minesweeper {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-gap: 20px;
+}
+
+.minesweeperSnippet {
+  max-width: 90%;
+  height: auto;
+  object-fit: cover;
+  image-rendering: pixelated;
+}
+
+.dfj-logo {
+  max-width: 300px;
+  max-height: 300px;
+}
+
+.ux-wireframe {
+  max-width: 400px;
+  max-height: 750px;
+}
+
+/* mobile */
+@media screen and (max-width: 1800px) {
   .project-flex {
     flex-direction: column;
     align-items: center;
@@ -179,50 +225,113 @@ export default {
   }
 }
 
-/*group of snippets from minesweeper project */
-.minesweeper {
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-  grid-gap: 20px;
+/* med sized screens */
+@media screen and (max-width: 1024px) {
+  .main {
+    padding: 0 16px;
+  }
+
+  .mx-20 {
+    margin-left: 16px;
+    margin-right: 16px;
+  }
 }
 
-/*one snippet */
-.minesweeperSnippet {
-  max-width: 90%;
-  height: auto;
-  object-fit: cover;
-  image-rendering: pixelated;
+/* tablet */
+@media screen and (max-width: 768px) {
+  .intro {
+    margin-inline: 24px;
+  }
+  
+  .dfj-logo {
+    max-width: 250px;
+    max-height: 250px;
+  }
+  
+  .profile {
+    width: 180px;
+    height: 180px;
+    margin-bottom: 20px;
+    min-width: 150px;
+  }
+  
+  .project-flex-item {
+    width: 90%;
+  }
 }
 
-/*for smaller screens */
-@media screen and (max-width:650px) {
+/* mobile */
+@media screen and (max-width: 650px) {
   .intro {
     flex-direction: column;
     align-items: center;
-    max-width: 600px;
-    margin: 0 auto;
-    margin-inline: 50px;
+    max-width: 100%;
+    margin: 24px 16px;
+    text-align: center;
     white-space: pre-line;
   }
-
+  
   .profile {
-    display: block;
-    margin: 0 auto;
-    width: 100%;
+    height: 150px;
+    width: 150px;
+    
+  }
+
+  .my-work {
+    margin-left: 0rem;
+    margin-right: 0rem;
+    margin-top: 0rem;
+  }
+  
+  .flex-col.ml-6 {
+    margin-left: 0; 
+  }
+  
+  h2.text-3xl {
+    font-size: 1.5rem;
+  }
+  
+  h2.text-2xl {
+    font-size: 1.25rem;
+  }
+  
+  .mx-20 {
+    margin-left: 12px;
+    margin-right: 12px;
+  }
+  
+  .dfj-logo {
     max-width: 200px;
-    height: auto;
-    border-radius: 50%;
+    max-height: 200px;
+  }
+  
+  /* project descriptions */
+  .project-flex-item {
+    width: 100%;
+  }
+  
+  hr {
+    margin: 24px 0;
   }
 }
 
-.dfj-logo {
-  max-width: 300px;
-  max-height: 300px;
-}
-
-.ux-wireframe {
-  max-width: 400px;
-  max-height: 750px;
-
+/* extra small */
+@media screen and (max-width: 480px) {
+  .profile {
+    width: 120px;
+    height: 120px;
+  } 
+  
+  .text-base {
+    font-size: 0.9rem;
+  }
+  
+  .minesweeper {
+    grid-template-columns: 1fr;
+  }
+  
+  .project-flex-item.ux-wireframe {
+    max-width: 280px;
+  }
 }
 </style>
